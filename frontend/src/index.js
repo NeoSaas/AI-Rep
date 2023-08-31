5ded6ae34c327b17392e945ea209169796d97bab
Aaron Perel <36312272+aabron@users.noreply.github.com>
2023-08-30 21:29:34 -0400

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
 import { AuthProvider } from './components/authentication/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
